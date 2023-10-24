855c57449a3d8dfc8b3a751e5272913437e7603a
damiano00 <pasquini.damiano00@gmail.com>
2023-10-24 19:00:19 +0000

    [done] finished implementing train.py [upload] mnist_cnn_model.h5

--- adversarial_ml/train.py
+++ adversarial_ml/train.py
@@ -1,2 +1,47 @@
+from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+# Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Preprocess the data
+x_train = x_train.reshape((60000, 28, 28, 1))
+x_train = x_train.astype('float32') / 255
+x_test = x_test.reshape((10000, 28, 28, 1))
+x_test = x_test.astype('float32') / 255
+
+# One-hot encode the labels
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# Define the CNN model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5, batch_size=64)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_acc * 100:.2f}%')
+
+# Check if the model meets the 98% accuracy threshold
+if test_acc >= 0.98:
+    # Save the model to a file
+    model.save('mnist_cnn_model.h5')
+    print("Model saved to 'mnist_cnn_model.h5'")
+else:
+    print("Model did not meet the accuracy threshold.")
